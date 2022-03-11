f2ed4d3b85e1c0eec69451870cf6545b7f95526c
Ngoc Nguyen Duy Minh <minhngocnguyenduy@gmail.com>
2022-03-11 18:32:54 +0700

    Add: Fonts & Colors
    Add the fonts & colors of the design system.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     './components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'full-typeface': ['Josefin Sans', 'sans-serif'],
+        regular: ['Lato', 'sans-serif'],
+      },
+      colors: {
+        pink: '#FB2E86',
+        'off-navy-blue': '#3F509E',
+        'navy-blue': '#151875',
+        purple: '#7E33E0',
+        'off-purple': '#9F63B5',
+        red: '#FB2448',
+        blue: '#2F1AC4',
+        'pantone-purple': '#E0D3F5',
+        'off-blue': '#151875',
+        'sky-blue': '#F3F9FF',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
